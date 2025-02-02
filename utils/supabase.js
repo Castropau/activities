b2710b218720a8fd8a47e17a30bf8563a06f6d16
Paulo Castro <castro.paulo5.00@gmail.com>
2025-02-02 20:01:11 +0800

    gumana ka

--- utils/supabase.js
+++ utils/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://jralndtkkmayksuckgcd.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyYWxuZHRra21heWtzdWNrZ2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxNjQ3NzYsImV4cCI6MjA1Mzc0MDc3Nn0.M1URkssBpDCy_A1Z9HKl2dxcnj0g2Q5Tb2v7r5m9fZQ'
-const supabase = createClient(supabaseUrl, supabaseKey);
+const Supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+export default Supabase;
